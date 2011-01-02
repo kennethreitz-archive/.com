6957eab00a118778b4166798bfb80f157ae01606
Kenneth Reitz <me@kennethreitz.com>
2011-01-02 16:53:10 -0500

    screw this

--- settings.py
+++ settings.py
@@ -7,14 +7,7 @@ GITHUB_URL = 'http://github.com/kennethreitz/'
 DISQUS_SITENAME = "kennethreitz"
 PDF_GENERATOR = False
 
-LINKS = (('Biologeek', 'http://biologeek.org'),
-            ('Filyb', "http://filyb.info/"),
-            ('Libert-fr', "http://www.libert-fr.com"),
-            ('N1k0', "http://prendreuncafe.com/blog/"),
-            (u'Tarek Ziadé', "http://ziade.org/blog"),
-            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+LINKS = ()
 
-SOCIAL = (('twitter', 'http://twitter.com/kennethreitz'),
-          ('flickr', 'http://flickr.com/photos/kennethreitz'),
-          ('github', 'http://github.com/kennethreitz'),)
+SOCIAL = ()
 
