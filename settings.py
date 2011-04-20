9e58a92fd2e31d7d1ad878453f2c81498ecd973c
Kenneth Reitz <me@kennethreitz.com>
2011-04-20 15:48:37 -0400

    improve

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 AUTHOR = u'Kenneth Reitz'
-SITENAME = u'Kenneth Reitz #3'
+SITENAME = u'Kenneth Reitz'
 SITEURL = 'http://kennethreitz.com/'
 
 GITHUB_URL = 'http://github.com/kennethreitz/'
@@ -9,12 +9,9 @@ PDF_GENERATOR = False
 
 BYLINE = '&copy; 2011 Kenneth Reitz &amp; co. All Rights Reserved.'
 LINKS = (
-	('GitHub Repos', 'http://github.com/kennethreitz'),
-	('Photography (Flickr)', 'http://flickr.com/kennethreitz'),
-	('Latest Tweets', 'http://twitter.com/kennethreitz'),
-	('R&eacute;sum&eacute;', 'http://www.linkedin.com/in/kennethreitz'),
-	('Design Portfolio', 'http://pick.im/kenneth-reitz'),
-	('Later Stars', 'http://laterstars.com/kennethreitz'),
+	('Colophon', '/about'),
+	('Projects', '/projects'),
+	('Articles', '/articles'),
 )
 
 SOCIAL = (
